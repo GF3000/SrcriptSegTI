1bd3c5da463f50026de189144230de746a0a36e4
GF3000 <94068411+GF3000@users.noreply.github.com>
2023-11-28 11:39:28 +0100

    Comentarios arreglados

--- Bloque3/graficas.py
+++ Bloque3/graficas.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
 import collections
 
-file = open("secreto_largo_AMEN.txt", "r")
-mensaje_cifrado = file.read()
-file.close()
+file1 = open("Pruebas/secreto_largo_AMEN.txt", "r")
+file2 = open("Pruebas/secreto_largo_preprocesado.txt", "r")
 
-# Make an histogram of the frequencies of the letters
-frecuencias = collections.Counter(mensaje_cifrado)
-#Sort keys
-frecuencias = collections.OrderedDict(sorted(frecuencias.items()))
-#Set transparency
-plt.bar(frecuencias.keys(), frecuencias.values(), alpha=0.5)
+mensaje_cifrado = file1.read()
+file1.close()
 
+# Primera gráfica
 
-
-file = open("secreto_largo_preprocesado.txt", "r")
-mensaje_cifrado = file.read()
-file.close()
-
-# Make an histogram of the frequencies of the letters
 frecuencias = collections.Counter(mensaje_cifrado)
 frecuencias = collections.OrderedDict(sorted(frecuencias.items()))
 plt.bar(frecuencias.keys(), frecuencias.values(), alpha=0.5)
+
+# Segunda gráfica
+
+mensaje_cifrado = file2.read()
+file2.close()
+
+frecuencias = collections.Counter(mensaje_cifrado)
+frecuencias = collections.OrderedDict(sorted(frecuencias.items()))
+plt.bar(frecuencias.keys(), frecuencias.values(), alpha=0.5)
+
+# Muestra las gráficas
 plt.show()
